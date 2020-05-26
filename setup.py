4b47f6ce89ae918ab9d7e0f5bed0297f48f42101
Radega12 <rauldearriba@gmail.com>
2020-05-26 10:20:08 +0200

    packaging application

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name='CodeStructureMaker_v1',
+setup(
+    name='codestructuremaker',
     version='1.0',
     description='Software to generade structures',
+    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Raül de Arriba',
     author_email='rauldearriba@gmail.com',
     url='https://github.com/Radega1993/CodeStructureMaker',
-    packages=setuptools.find_packages(),
-    setup_requires=['wheel'],
+    packages=find_packages(),
+    python_requires='>=3.6',
+    scripts=["app.py"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
 )
